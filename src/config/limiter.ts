0c1ddd6f9d09a99f7379bb11957f21375b877893
Gustavo <gustavolopez819@gmail.com>
2026-02-11 18:40:34 -0500

    commit

--- src/config/limiter.ts
+++ src/config/limiter.ts
@@ -2,6 +2,6 @@ import { rateLimit } from "express-rate-limit";
 
 export const limiter = rateLimit({
     windowMs: 60 * 1000,
-    limit: 10,
+    limit: 20,
     message: {"error": "Alcanzaste el limite de peticiones"}
 })
