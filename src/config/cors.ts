60aede05cd5ea504f3842b625a026f1751ec6f37
Gustavo <gustavolopez819@gmail.com>
2026-02-18 18:52:55 -0500

    cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,15 +4,6 @@ export const corsConfig: CorsOptions = {
   origin: (origin, callback) => {
     const whiteList = [process.env.FRONTEND_URL].filter(Boolean);
 
-    if (process.env.NODE_ENV === "development" || process.argv[2] === "--api") {
-      whiteList.push(
-        undefined,
-        "http://localhost:3000",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:3001",
-      );
-    }
 
     if (!origin || whiteList.includes(origin)) {
       callback(null, true);
