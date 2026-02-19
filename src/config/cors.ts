2b6e6f4b7cf831d245e7f9973d7f64aa03ee8672
Gustavo <gustavolopez819@gmail.com>
2026-02-18 21:37:21 -0500

    commit

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,15 +4,11 @@ export const corsConfig: CorsOptions = {
   origin: (origin, callback) => {
     const whiteList = [process.env.FRONTEND_URL].filter(Boolean);
 
-
-    if (!origin || whiteList.includes(origin)) {
+    if (origin && whiteList.includes(origin)) {
       callback(null, true);
     } else {
       console.error(`CORS Error: Origin ${origin} not allowed`);
       callback(new Error(`Error de CORS: Origen ${origin} no permitido`));
     }
   },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"],
 };
